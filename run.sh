9e7739a186be0e11ce659000358f99823af7c083
GXVT Lab <gxvt@bbn.com>
2019-09-11 11:29:33 -0400

    removing unnecessary args from run

--- run.sh
+++ run.sh
@@ -3,7 +3,5 @@ xhost +local:root
 nvidia-docker run -it \
     --env="DISPLAY" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="/tmp/mavlink:/tmp/mavlink" \
-    -p5760:5760 -p5761:5761 --env HOST_IP=192.168.1.181 \
     turtlebot
 xhost -local:root
